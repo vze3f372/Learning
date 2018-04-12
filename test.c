20dd7f0ffa08eeced44deed128fd3faff490744c
Joseph Hotchkiss <josephhotchkiss@Josephs-MacBook-Pro.local>
2018-04-12 13:57:07 +0300

    Does this work

--- test.c
+++ test.c
@@ -1,10 +1,21 @@
 
 
 #include<stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+
+	int* y;
+//	y = malloc(sizeof(int));
+	*y = 13;
 
 
-int main(void){
+	printf("%d", *y);
 
-	printf("Hello World\n");
-		//return 42;
-	}
+//	free(y); 
+	return 0;
+
+}
+
+
+
